56f776ae92d0f53c40a523e5345cb101dee08ef6
ValentinGuitton <94304960+ValentinGuitton@users.noreply.github.com>
2021-12-29 18:44:21 +0100

    Add files via upload

---
+++ Export_projet/projet_lo02/src/frr/utt/lo02/projet/Aléatoire.java
@@ -0,0 +1,9 @@
+package frr.utt.lo02.projet;
+
+import frr.utt.lo02.projet.Modele.Stratégie;
+
+public class Aléatoire implements Stratégie {
+    public void jouerBot() {
+    }
+
+}
---
+++ Export_projet/projet_lo02/src/frr/utt/lo02/projet/EffetWitch.java
@@ -0,0 +1,7 @@
+package frr.utt.lo02.projet;
+
+public class EffetWitch {
+    public void activerEffet(final String nom) {
+    }
+
+}
